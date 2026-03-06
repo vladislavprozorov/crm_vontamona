bb0969231b47a01e34e15b369f3bf885d5dfaaea
vladislavprozorov <slawasam@mail.ru>
2026-03-05 21:04:55 -0800

    chore(package): add type module to package json

--- client/next.config.ts
+++ client/next.config.ts
@@ -5,3 +5,16 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+
+export function toDevServerHref(base: string, url: string){
+    if(url.startsWith('/')){
+      return `${base}${url.slice(1)}`
+    }
+}
+
+describe("normal css file with base /", () => {
+  test("ok", () => {
+    expect(toDevServerHref("http://localhost:3000/", "/api/test")).toBe("http://localhost:3000/api/test")
+  })
+})
